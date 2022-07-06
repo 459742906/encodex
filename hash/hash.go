88f2fc9f132a50310ea9e43d2899ec3b5597f7cf
陈辉 <chenhui@weimiao.cn>
2022-07-06 19:07:10 +0800

    init2

--- hash/hash.go
+++ hash/hash.go
@@ -1,1 +1,15 @@
 package hash
+
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func Md5(str string) string {
+	h :=md5.New()
+	h.Write([]byte(str))
+	re := h.Sum(nil)
+	md5Str := fmt.Sprintf("%#v",re)
+
+	return md5Str
+}
